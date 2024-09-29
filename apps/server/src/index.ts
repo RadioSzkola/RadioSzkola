10ecefb85722480266aaab9beeafea9790f28b98
kiju <kijekkuba2006@gmail.com>
2024-09-29 11:53:43 +0200

    Setup nuxt with tailwind

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -8,7 +8,7 @@ app.get("/", c => {
     return c.text(helloWorld());
 });
 
-const port = 3000;
+const port = 8080;
 console.log(`Server is running on port ${port}`);
 
 serve({
---
+++ apps/web/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const colorMode = useColorMode();
+
+function handleSwitchColorTheme() {
+    if (colorMode.value === "dark") {
+        colorMode.preference = "light";
+    } else {
+        colorMode.preference = "dark";
+    }
+}
+</script>
+
+<template>
+    <h1>{{ colorMode.value }}</h1>
+    <button v-on:click="handleSwitchColorTheme">Switch the theme!</button>
+</template>
