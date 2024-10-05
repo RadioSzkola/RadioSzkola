cc10440e9bc668309f46da0db1202c1ed073d384
kiju <kijekkuba2006@gmail.com>
2024-10-05 15:32:29 +0200

    Setup drizzle studio

--- apps/server/drizzle.config.ts
+++ apps/server/drizzle.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "drizzle-kit";
-import { DATABASE_MIGRATIONS_FOLDER } from "./src/const";
+import { DATABASE_MIGRATIONS_FOLDER, DATABASE_PATH } from "./src/const";
 
 export default defineConfig({
     schema: "./src/schema.ts",
     out: DATABASE_MIGRATIONS_FOLDER,
     dialect: "sqlite",
+    dbCredentials: {
+        url: DATABASE_PATH,
+    },
 });
