1afa68117a539388dcd78c5fdfddb436b5084e6b
MilkyWayHippopotamus <github.elaborate284@passmail.net>
2025-01-12 18:44:50 +0100

    Fixed frontend for sign-up and sign-in

--- apps/web/src/ui/signup.tsx
+++ apps/web/src/ui/signup.tsx
@@ -8,9 +8,10 @@ export default function SignUp() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [schoolId, setSchoolId] = useState("");
-    const [passwordVisibility, setPasswordVisibility] = useState("");
-    const [visibility, setVisibility] = useState(false);
-    const [signUpOrIn, setSignUpOrIn] = useState(1);
+    const [passwordVisibility, setPasswordVisibility] = useState<
+        "password" | "text"
+    >("text");
+    const [signUpOrIn, setSignUpOrIn] = useState<0 | 1>(1);
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
@@ -38,6 +39,15 @@ export default function SignUp() {
         document
             .getElementById("signup-container")
             ?.classList.add(styles.signVisibilityContainerHidden);
+
+        setEmail("");
+        setPassword("");
+        setSchoolId("");
+        setPasswordVisibility("text");
+    };
+
+    const handleSignUpOrIn = async () => {
+        setSignUpOrIn(prevState => (prevState === 0 ? 1 : 0));
     };
 
     return (
@@ -59,13 +69,22 @@ export default function SignUp() {
                 {signUpOrIn === 1 ? (
                     <form
                         onSubmit={e => handleSubmit(e)}
-                        className={styles.signUpForm}
+                        className={styles.signForm}
                     >
                         <input
                             type="email"
+                            className={styles.signEmailInput}
                             placeholder="Email"
                             value={email}
                             onChange={e => setEmail(e.target.value)}
+                            required
+                        />
+                        <input
+                            type="text"
+                            className={styles.signSchoolIdInput}
+                            placeholder="ID użytkownika"
+                            value={schoolId}
+                            onChange={e => setSchoolId(e.target.value)}
                             required
                         />
                         <div className={styles.passwordContainer}>
@@ -75,6 +94,7 @@ export default function SignUp() {
                                         ? "text"
                                         : "password"
                                 }
+                                className={styles.signPasswordInput}
                                 placeholder="Hasło"
                                 value={password}
                                 onChange={e => setPassword(e.target.value)}
@@ -82,6 +102,7 @@ export default function SignUp() {
                             />
                             <input
                                 type="checkbox"
+                                className={styles.showPasswordCheckbox}
                                 id="showPassword"
                                 checked={passwordVisibility === "password"}
                                 onChange={() =>
@@ -92,54 +113,111 @@ export default function SignUp() {
                                     )
                                 }
                             />
+                            <label
+                                htmlFor="showPassword"
+                                className={styles.showPasswordCheckboxLabel}
+                            >
+                                {passwordVisibility === "text"
+                                    ? "Pokaż"
+                                    : "Ukryj"}{" "}
+                                hasło
+                            </label>
                         </div>
-                        <input
-                            type="text"
-                            placeholder="ID użytkownika"
-                            value={schoolId}
-                            onChange={e => setSchoolId(e.target.value)}
-                            required
-                        />
-                        <label htmlFor="showPassword">Pokaż hasło</label>
-                        <button type="submit">Zarejestruj się</button>
+                        <button
+                            type="submit"
+                            id="signUp"
+                            className={styles.signButton}
+                        >
+                            Zarejestruj się
+                        </button>
                     </form>
                 ) : (
                     <form
                         onSubmit={e => handleSubmit(e)}
-                        className={styles.signInForm}
+                        className={styles.signForm}
                     >
                         <input
                             type="email"
+                            className={styles.signEmailInput}
                             placeholder="Email"
                             value={email}
                             onChange={e => setEmail(e.target.value)}
                             required
                         />
                         <input
-                            type={
-                                passwordVisibility === "password"
-                                    ? "text"
-                                    : "password"
-                            }
-                            placeholder="Hasło"
-                            value={password}
-                            onChange={e => setPassword(e.target.value)}
-                            required
-                        />
-                        <label htmlFor="showPassword">Pokaż hasło</label>
-                        <input
                             type="text"
+                            className={styles.signSchoolIdInput}
                             placeholder="ID użytkownika"
                             value={schoolId}
                             onChange={e => setSchoolId(e.target.value)}
                             required
                         />
-                        <button type="submit">Zaloguj się</button>
+                        <div className={styles.passwordContainer}>
+                            <input
+                                type={
+                                    passwordVisibility === "password"
+                                        ? "text"
+                                        : "password"
+                                }
+                                className={styles.signPasswordInput}
+                                placeholder="Hasło"
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}
+                                required
+                            />
+                            <input
+                                type="checkbox"
+                                className={styles.showPasswordCheckbox}
+                                id="showPassword"
+                                checked={passwordVisibility === "password"}
+                                onChange={() =>
+                                    setPasswordVisibility(
+                                        passwordVisibility === "password"
+                                            ? "text"
+                                            : "password",
+                                    )
+                                }
+                            />
+                            <label
+                                htmlFor="showPassword"
+                                className={styles.showPasswordCheckboxLabel}
+                            >
+                                {passwordVisibility === "text"
+                                    ? "Pokaż"
+                                    : "Ukryj"}{" "}
+                                hasło
+                            </label>
+                        </div>
+                        <button
+                            type="submit"
+                            id="signIn"
+                            className={styles.signButton}
+                        >
+                            Zaloguj się
+                        </button>
                     </form>
                 )}
-                <span>
-                    Nie masz konta? <button>Zarejestruj się!</button>
-                </span>
+                {signUpOrIn === 1 ? (
+                    <span className={styles.toggleSignUpIn}>
+                        Nie masz konta?{" "}
+                        <button
+                            className={styles.toggleSignUpInButton}
+                            onClick={handleSignUpOrIn}
+                        >
+                            Zaloguj się!
+                        </button>
+                    </span>
+                ) : (
+                    <span className={styles.toggleSignUpIn}>
+                        Nie masz konta?{" "}
+                        <button
+                            className={styles.toggleSignUpInButton}
+                            onClick={handleSignUpOrIn}
+                        >
+                            Zarejestruj się!
+                        </button>
+                    </span>
+                )}
             </div>
         </div>
     );
