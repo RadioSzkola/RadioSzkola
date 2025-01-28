20ac5d1aa41ac4283317593c2941fa877f6c8c49
kijudev <dev@kiju.page>
2025-01-28 19:02:59 +0100

    migrate

--- apps/server/migrations/0000_perfect_vulture.sql
+++ apps/server/migrations/0000_perfect_vulture.sql
@@ -1,5 +1,14 @@
+CREATE TABLE `authId` (
+	`id` text PRIMARY KEY NOT NULL,
+	`userId` text,
+	`expiresAt` integer NOT NULL,
+	`createdAt` integer NOT NULL,
+	`updatedAt` integer NOT NULL,
+	FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
 CREATE TABLE `school` (
-	`id` integer PRIMARY KEY NOT NULL,
+	`id` text PRIMARY KEY NOT NULL,
 	`city` text NOT NULL,
 	`adress` text NOT NULL,
 	`name` text NOT NULL,
@@ -11,6 +20,8 @@ CREATE TABLE `session` (
 	`id` text PRIMARY KEY NOT NULL,
 	`user_id` text NOT NULL,
 	`expires_at` integer NOT NULL,
+	`createdAt` integer NOT NULL,
+	`updatedAt` integer NOT NULL,
 	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
@@ -20,7 +31,7 @@ CREATE TABLE `user` (
 	`name` text NOT NULL,
 	`email` text NOT NULL,
 	`passwordHash` text NOT NULL,
-	`schoolId` integer NOT NULL,
+	`schoolId` text NOT NULL,
 	`createdAt` integer NOT NULL,
 	`updatedAt` integer NOT NULL,
 	FOREIGN KEY (`schoolId`) REFERENCES `school`(`id`) ON UPDATE no action ON DELETE no action
